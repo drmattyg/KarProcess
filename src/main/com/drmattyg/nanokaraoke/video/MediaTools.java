4fb5ac3bb4dbd4e7630a8ccadc12391b27a5d674
Matthew Gordon <mgordon@palantir.com>
2015-06-20 09:03:38 -0700

    Adding functionality for adding audio at an offset; not yet working

--- src/main/com/drmattyg/nanokaraoke/video/MediaTools.java
+++ src/main/com/drmattyg/nanokaraoke/video/MediaTools.java
@@ -1,40 +1,38 @@
 package com.drmattyg.nanokaraoke.video;
 
-import java.awt.AlphaComposite;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.TimeUnit;
 
 import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.MediaToolAdapter;
-import com.xuggle.mediatool.ToolFactory;
 import com.xuggle.mediatool.event.IVideoPictureEvent;
 
 public class MediaTools {
 	
 	public static class VideoCutter extends MediaToolAdapter {
 
-		private VideoCutter() {};
-		private long startTime;
-		private long duration;
-		private IMediaWriter mediaWriter;
-		private String outputFile;
-		private long fadeTime;
-		public static VideoCutter getInstance(long startTimeMillis, long lengthMillis, String outputFile) {
+		protected VideoCutter() {};
+		protected long startTime;
+		protected long duration;
+		protected IMediaWriter mediaWriter;
+		protected long fadeTime;
+		protected boolean startVideo = false;
+		protected boolean endFade = false;
+		public static VideoCutter getInstance(long startTimeMillis, long lengthMillis, IMediaWriter writer) {
 			VideoCutter vc = new VideoCutter();
 			vc.startTime = startTimeMillis;
 			vc.duration = lengthMillis;
-			vc.outputFile = outputFile;
+			vc.mediaWriter = writer;
 			return vc;
 		}
 		
-		public static VideoCutter getInstance(long startTimeMillis, long lengthMillis, long fadeTime, String outputFile) {
+		public static VideoCutter getInstance(long startTimeMillis, long lengthMillis, long fadeTime, IMediaWriter writer) {
 			VideoCutter vc = new VideoCutter();
 			vc.startTime = startTimeMillis;
 			vc.duration = lengthMillis;
-			vc.outputFile = outputFile;
 			vc.fadeTime = fadeTime;
+			vc.mediaWriter = writer;
 			return vc;
 		}
 		
@@ -44,12 +42,13 @@ public class MediaTools {
 			if(timestamp > startTime && timestamp < (startTime + duration)) {
 				BufferedImage img = event.getImage();
 				if(timestamp < startTime + fadeTime) {
+					startVideo = true;
 					float fadePercent = (timestamp - startTime)*1.0f/fadeTime;
 					img = ImageUtils.fade(img, fadePercent);
 				} else if (timestamp > startTime + duration - fadeTime) {
 					float fadePercent = (duration - (timestamp - startTime))*1f/fadeTime;
 					img = ImageUtils.fade(img, fadePercent);
-				}
+				} else if(startVideo) endFade = true;
 				
 				mediaWriter.encodeVideo(0, img, timestamp - startTime, TimeUnit.MILLISECONDS);
 			}
@@ -59,9 +58,8 @@ public class MediaTools {
 		public void cutVideo(IMediaReader mediaReader) {
 			mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
 			mediaReader.addListener(this);
-			mediaWriter = ToolFactory.makeWriter(outputFile, mediaReader);
 			while (mediaReader.readPacket() == null);
-			mediaWriter.close();
+//			mediaWriter.close();
 		}
-	}
+	}	
 }
--- src/main/com/drmattyg/nanokaraoke/video/VideoTest.java
+++ src/main/com/drmattyg/nanokaraoke/video/VideoTest.java
@@ -1,24 +1,71 @@
 package com.drmattyg.nanokaraoke.video;
 
+import java.util.concurrent.TimeUnit;
+
 import com.drmattyg.nanokaraoke.video.MediaTools.VideoCutter;
 import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.MediaToolAdapter;
 import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.mediatool.event.IAudioSamplesEvent;
+import com.xuggle.xuggler.IAudioSamples;
+import com.xuggle.xuggler.IAudioSamples.Format;
+import com.xuggle.xuggler.IContainer;
 import com.xuggle.xuggler.IRational;
 
 
 public class VideoTest {
 	public static String videoFile = "/Users/mgordon/test/tb.mp4";
 	public static String output = "/Users/mgordon/test/tb_out.mp4";
-	private static IMediaWriter mediaWriter;
-	private static IRational tb = null;
+	public static String audio = "/Library/Java/Demos/Sound/JavaSoundDemo/audio/1-welcome.wav";
 
+	// timidity -Ow -o output.wav input.kar
+	IMediaReader ar = ToolFactory.makeReader(audio);
+	
 	static long startTime = 10000; // 10 seconds
-	static long duration = 20000; // 15 seconds
+	static long duration = 40000; // 15 seconds
 	public static void main(String[] args) {
 		IMediaReader mediaReader = ToolFactory.makeReader(videoFile);
-		VideoCutter vc = VideoCutter.getInstance(startTime, duration, 5000, output);
+		IMediaWriter writer = ToolFactory.makeWriter(output);
+		VideoCutter vc = VideoCutter.getInstance(startTime, duration, 5000, writer);
 		vc.cutVideo(mediaReader);
 		
+		IMediaReader au = ToolFactory.makeReader(audio);
+		IContainer auContainer = IContainer.make();
+		auContainer.open(audio, IContainer.Type.READ, null);
+//		int numStreams = auContainer.getNumStreams();
+		int numChannels = auContainer.getStream(0).getStreamCoder().getChannels();
+		int sampleRate = auContainer.getStream(0).getStreamCoder().getSampleRate();
+		writer.addAudioStream(0, 0, numChannels, sampleRate);
+//		System.out.printf("%d, %d, %d\n", numStreams, numChannels, bitrate);
+		AddAudio aa = new AddAudio();
+		aa.writer = writer;
+		au.addListener(aa);
+		while(au.readPacket() == null);
+		writer.close();
+		
+		
+		
+		
 	}
+	
+	static class AddAudio extends MediaToolAdapter {
+		public long startTime = 15000;
+		private static final IRational myBase = IRational.make(1, 1000); // millis
+		public IMediaWriter writer;
+		@Override
+		public void onAudioSamples(IAudioSamplesEvent event) {
+//			long samples[] = new long[(int) event.getMediaData().getNumSamples()];
+//			for(int i = 0; i < event.getMediaData().getNumSamples(); i++) samples[i] = event.getMediaData().getSample(i, 0, Format.FMT_NONE);
+			IAudioSamples s = event.getAudioSamples();
+			IRational tb = s.getTimeBase();
+			long timestamp = event.getTimeStamp() + tb.rescale(startTime, myBase);
+			s.setTimeStamp(timestamp);
+			writer.encodeAudio(0, s);
+			super.onAudioSamples(event);
+		} 
+		
+	}
+
+	
 }
