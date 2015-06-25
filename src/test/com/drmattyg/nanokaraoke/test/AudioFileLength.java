a6a2f138f23769d117845183ed703eb077673c9a
Matthew Gordon <mgordon@palantir.com>
2015-06-25 13:00:50 -0700

    Adding some actual tests for real, instead of just screwing around with test files.

---
+++ src/test/com/drmattyg/nanokaraoke/test/AudioFileLength.java
@@ -0,0 +1,28 @@
+package com.drmattyg.nanokaraoke.test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.junit.Test;
+
+import com.drmattyg.nanokaraoke.video.MediaTools;
+
+
+public class AudioFileLength {
+
+	@Test
+	public void test() {
+		System.out.println(System.getProperty("user.dir"));
+		File f = new File("src/main/resources/bridge.wav");
+		assertTrue(f.exists() && f.canRead());
+		try { 
+			assertTrue(MediaTools.getWavFileDuration(f) > 0);
+		} catch(IOException | UnsupportedAudioFileException ex) {
+			fail(ex.getMessage());
+		}
+	}
+
+}
---
+++ src/test/com/drmattyg/nanokaraoke/test/VideoProcessor.java
@@ -0,0 +1,15 @@
+package com.drmattyg.nanokaraoke.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class VideoProcessor {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
