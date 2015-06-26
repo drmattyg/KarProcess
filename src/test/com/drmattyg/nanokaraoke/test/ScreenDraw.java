8a0d71f70fd7587be2f50e0a88c93697a73950a3
Matthew Gordon <mgordon@palantir.com>
2015-06-26 11:16:33 -0700

    Committing the screen draw test

---
+++ src/test/com/drmattyg/nanokaraoke/test/ScreenDraw.java
@@ -0,0 +1,51 @@
+package com.drmattyg.nanokaraoke.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.junit.Test;
+
+import com.drmattyg.nanokaraoke.MidiFile;
+import com.drmattyg.nanokaraoke.video.KaraokeLine;
+import com.drmattyg.nanokaraoke.video.KaraokeScreen;
+
+public class ScreenDraw {
+
+	private static String IMAGE_FILE = "src/test/resources/cheetahs.jpg";
+	private static final String MIDI_FILE = "src/test/resources/bridge_over_troubled_water.kar";
+
+	@Test
+	public void test() {
+		try {
+			BufferedImage img = ImageIO.read(new File(IMAGE_FILE));
+			MidiFile mf = MidiFile.getInstance(MIDI_FILE);
+			KaraokeScreen sc = KaraokeScreen.getInstance(img, KaraokeLine.toKaraokeLines(mf), 4, 5, 1);
+			BufferedImage b = sc.render();
+			JFrame f = new JFrame();
+			f.setSize(b.getWidth(), b.getHeight());
+			ImageIcon icon = new ImageIcon(b);
+			JLabel l = new JLabel(icon);
+			JPanel p = new JPanel();
+			p.add(l);
+			f.add(p);
+			f.setVisible(true);
+			Thread.sleep(10000);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
